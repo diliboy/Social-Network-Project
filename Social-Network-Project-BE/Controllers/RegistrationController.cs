5949c8e138acb45788dc16002edfd1d24c1e14fe
Dilan Bandara <dilan.harshana.739@gmail.com>
2025-06-23 23:53:35 +1200

    Added Registration Controller

---
+++ Social-Network-Project-BE/Controllers/RegistrationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Social_Network_Project_BE.Models;
+
+namespace Social_Network_Project_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Registration")]
+
+        public Response Registration(Registration registration)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SNCon").ToString());
+
+            Dal dal = new Dal();
+            response = dal.Registartion(registration, connection);
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+
+        public Response Login(Registration registration)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SNCon").ToString());
+            Dal dal = new Dal();
+            response = dal.Login(registration, connection);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("UserApproval")]
+
+        public Response UserApproval(Registration registration, int Id)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SNCon").ToString());
+            Dal dal = new Dal();
+            response = dal.UserApproval(registration, connection);
+            return response;
+        }
+    }
+}
---
+++ Social-Network-Project-BE/Models/Dal.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Social_Network_Project_BE.Models
+{
+    public class Dal
+    {
+        public Response Registartion(Registration registration, SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(Name,Email,Password,PhoneNo,IsActive,IsApproved)" +
+                "VALUES('" + registration.Name + "','" + registration.Email + "','" + registration.Password + "','" + registration.PhoneNo + "',1,0)", connection);
+
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "Registration Successful";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "Registration Failed";
+            }
+
+            return response;
+        }
+
+        public Response Login(Registration registration, SqlConnection connection)
+        {
+            Response response = new Response();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Registration WHERE Email = '" + registration.Email + "' AND Password ='" + registration.Password + "' ", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+
+
+            if (dt.Rows.Count > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "Login Successful";
+
+                Registration reg = new Registration();
+                reg.Id = Convert.ToInt32(dt.Rows[0]["Id"]);
+                reg.Name = Convert.ToString(dt.Rows[0]["Name"]);
+                reg.Email = Convert.ToString(dt.Rows[0]["Email"]);
+
+                response.Registration = reg;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "Login Failed";
+
+                response.Registration = null;
+            }
+            return response;
+        }
+
+        public Response UserApproval(Registration registration, SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("UPDATE Registration SET IsApproved = 1 WHERE Id = '" + registration.Id + "' AND IsActive =1", connection);
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "User Approved Successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User Approval Failed";
+            }
+            return response;
+        }
+
+        public Response News(News news, SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("INSERT INTO News(Title,Content,Email,IsActive,CreatedOn)" +
+                "VALUES('" + news.Title + "','" + news.Content + "','" + news.Email + "','1', GETDATE())", connection);
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "News Created Successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "News Creation Failed";
+            }
+            return response;
+        }
+
+        public Response NewsList(SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM News WHERE IsActive = 1", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                response.listNews = new List<News>();
+                foreach (DataRow row in dt.Rows)
+                {
+                    News news = new News
+                    {
+                        Id = Convert.ToInt32(row["Id"]),
+                        Title = Convert.ToString(row["Title"]),
+                        Content = Convert.ToString(row["Content"]),
+                        Email = Convert.ToString(row["Email"]),
+                        IsActive = Convert.ToInt32(row["IsActive"]),
+                        CreatedOn = Convert.ToString(row["CreatedOn"])
+                    };
+                    response.listNews.Add(news);
+                }
+                response.StatusCode = 200;
+                response.StatusMessage = "News Retrieved Successfully";
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "No News Found";
+                response.listNews = null;
+            }
+            return response;
+        }
+    }
+}
